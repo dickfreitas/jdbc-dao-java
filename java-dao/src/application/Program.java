5b37e630a26a246cea18ed78f0c2d093e94b2a8b
Dickson Freitas <dickfreitas12@gmaill.com>
2024-03-20 18:46:57 -0300

    FindByDepartment

--- java-dao/src/application/Program.java
+++ java-dao/src/application/Program.java
@@ -5,6 +5,8 @@ import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
         SellerDAO sellerDAO = DaoFactory.createSellerDao();
@@ -13,5 +15,12 @@ public class Program {
         Seller seller = sellerDAO.findById(3);
 
         System.out.println(seller);
+
+        System.out.println("\n----FindByDepartment------");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDAO.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
